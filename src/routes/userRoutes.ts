fd67889af631d580a57f7ad0fe06344c7c1b4457
Nirmit Kaundal <nirmit.kaundal@nirmitvrs-25.successivetech.com>
2025-07-16 16:58:00 +0530

    feat:some question are implemented

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { showData, storeData } from "../controller/userController";
+import { checkSchema, showData, storeData } from "../controller/userController";
 import { validateUser, validateUserJwt } from "../middleware/validateUser";
+import { validateSchema } from "../middleware/validateSchema";
+import { validateQuery } from "../middleware/validateQuery";
 // import { loggerFunction } from "../middleware/loggerFunction";
 
 
@@ -8,7 +10,9 @@ import { validateUser, validateUserJwt } from "../middleware/validateUser";
 const router = Router();
 
 router.get("/userdata",validateUserJwt,showData);
-router.post("/storedata",validateUser,storeData)
+router.post("/storedata",validateUser,storeData);
+router.post("/usercredentials",validateSchema,checkSchema);
+router.get("/fetchid/:id",validateQuery)
 
 
 
